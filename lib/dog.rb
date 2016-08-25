30c7eb28413ed2b3829322425e269e77a0938408
Tedman Hsu <twh26@cornell.edu>
2016-08-25 00:41:11 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+end
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
+
+class Person
+end
+alan_kay = Person.new
+adele_goldberg = Person.new
---
+++ lib/person.rb
@@ -0,0 +1,4 @@
+class Person
+end
+alan_kay = Person.new
+adele_goldberg = Person.new
