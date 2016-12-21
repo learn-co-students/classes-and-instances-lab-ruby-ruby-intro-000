e3d73efc314941d57c2ec14485b6f2da6803106f
Salvatore McCarty <salvatore.mccarty@gmail.com>
2016-12-21 22:41:40 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+#Here is my Dog class
+class Dog
+  #Doggy Details to be determined (&deflea'd)
+  
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
---
+++ lib/person.rb
@@ -0,0 +1,6 @@
+#Person Definition of Class
+class Person
+end
+
+adele_goldberg = Person.new
+alan_kay = Person.new
