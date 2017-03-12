140f3beb0c6ddfdb4183aab2d7690dfc681d1a15
Paul Arias <p.anthony.arias@gmail.com>
2017-03-12 20:24:04 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
 end
 
-Dog.new = fido
-Dog.new = snoopy
-Dog.new = lassie
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
 end
 
-Person.new = adele_goldberg
-Person.new = alan_kay
+adele_goldberg = Person.new
+alan_kay = Person.new
