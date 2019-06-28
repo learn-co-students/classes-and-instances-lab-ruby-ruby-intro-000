f4cb323370dded0b6ce027b53ee38e86d429599e
Izreel James <izreeljames@gmail.com>
2019-06-28 17:06:09 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog 
+  f_dName = 'fiddo'
+end
+
+class Dog
+  s_dName = 'Snoopy'
+end
+
+class Dog
+  t_dName = 'Lassie'
+end
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+  f_pName = 'adele_goldberg'
+end
+
+class Person
+  s_pName = 'alan_kay'
+end 
